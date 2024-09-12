c2ae8ce2afa412761c50d570247d7d4f40f5d70c
Dzaky Waly <andarwaly@gmail.com>
2024-09-12 15:45:57 +0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Muhammad Dzaky on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
